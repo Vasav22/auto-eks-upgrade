d4740b7d0a171fb799d8bc9394cf392f449ef40c
Karthikeyan Sundararajan <karthikeyan.sundararajan@opsera.io>
2025-06-23 15:17:24 +0000

    OPL-25283: Update cleanup cronjob to use exclude list

--- tools-cleanup-cronjob/install.sh
+++ tools-cleanup-cronjob/install.sh
@@ -1,1 +1,8 @@
-helm upgrade --install tools-cleanup-cronjob . -n infra-cleanup --create-namespace
+# Test
+# helm upgrade --install tools-cleanup-cronjob . -n infra-cleanup --create-namespace -f values-test.yaml
+
+# UAT
+# helm upgrade --install tools-cleanup-cronjob . -n infra-cleanup --create-namespace -f values-uat.yaml
+
+# ProdQA
+# helm upgrade --install tools-cleanup-cronjob . -n infra-cleanup --create-namespace -f values-prodqa.yaml
